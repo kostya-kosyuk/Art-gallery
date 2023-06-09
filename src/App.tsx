07f3df9eefca3c1935d06eb2bf7f40185bbc51dd
Kostya Kosyuk <kostya.kosyuk007@gmail.com>
2023-06-09 22:37:40 +0300

    removed MUI, installed sass

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,37 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
-import { Box, ThemeProvider, createTheme } from '@mui/material';
 import Artists from './pages/Artists';
 import Artworks from './pages/Artworks';
 import Checkout from './pages/Checkout';
 import Favorites from './pages/Favorites';
 import PageNotFound from './pages/PageNotFound';
 import ArtWorkPage from './pages/ArtworkPage';
-import Header from './components/Header/Header';
-
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#007bff',
-    },
-    secondary: {
-      main: '#6c757d',
-    },
-  },
-});
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <Box
-        sx={{
-          position: 'relative',
-        }}
-      >
+        <>
+          <header />
 
-        <Header />
+          <main>
+            <div>
+              <Routes>
+                <Route path="/" element={<Artworks />} />
+                <Route path="home" element={<Navigate to="/" replace />} />
 
-        <Box component='main'>
-          <div>
-            <Routes>
-              <Route path="/" element={<Artworks />} />
-              <Route path="home" element={<Navigate to="/" replace />} />
+                <Route path="artists" element={<Artists />} />
 
-              <Route path="artists" element={<Artists />} />
+                <Route path="artworks/artwork" element={<ArtWorkPage />} />
 
-              <Route path="artworks/artwork" element={<ArtWorkPage />} />
+                <Route path="checkout" element={<Checkout />} />
 
-              <Route path="checkout" element={<Checkout />} />
+                <Route path="favourites" element={<Favorites />} />
 
-              <Route path="favourites" element={<Favorites />} />
+                <Route path="*" element={<PageNotFound />} />
+              </Routes>
+            </div>
+          </main>
 
-              <Route path="*" element={<PageNotFound />} />
-            </Routes>
-          </div>
-        </Box>
-
-        {/* <Footer /> */}
-      </Box>
-      </ ThemeProvider>
+          {/* <Footer /> */}
+        </>
   );
 }
 
--- src/components/Header/components/NavButton.tsx
+++ src/components/Header/components/NavButton.tsx
@@ -1,25 +1,24 @@
-import { ListItem, Theme, styled } from "@mui/material";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
-const StyledNavButton = styled(ListItem)(({ theme }: { theme: Theme }) => ({
-    '&:hover': {
-        color: theme.palette.primary.main,
-    },
-}));
+// const StyledNavButton = styled(ListItem)(({ theme }: { theme: Theme }) => ({
+//     '&:hover': {
+//         color: theme.palette.primary.main,
+//     },
+// }));
 
-type Props = {
-    linkTo: string,
-    title: string,
-};
+// type Props = {
+//     linkTo: string,
+//     title: string,
+// };
 
-const CustomNavButton = ({linkTo, title}: Props) => {
-    return (
-        <StyledNavButton>
-            <Link to={linkTo}>
-                {title}
-            </Link>
-        </StyledNavButton>
-    );
-};
+// const CustomNavButton = ({linkTo, title}: Props) => {
+//     return (
+//         <StyledNavButton>
+//             <Link to={linkTo}>
+//                 {title}
+//             </Link>
+//         </StyledNavButton>
+//     );
+// };
 
-export default CustomNavButton;
+// export default CustomNavButton;
