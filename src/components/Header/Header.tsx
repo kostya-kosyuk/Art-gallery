f37df4679df2f860c4f3d6f9838cd9f80bdd872e
Kostya Kosyuk <kostya.kosyuk007@gmail.com>
2023-06-16 14:45:26 +0300

    finish. Full adaptive layout, fonts, index.scss

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,44 +4,103 @@ import header from './Header.module.scss';
 
 const Header = () => {
     return (
-        <header className={header.header}>
-            <div className={header.logo}>
-                <Link
-                    to={'/'}
-                    className={header.logo__link}
-                >
-                    <img
-                        className={header.logo__link__image}
-                        src="https://onlinegallery.art/_webblocks/images/logo.svg"
-                        alt="logo"
-                    />
-                </Link>
-            </div>
-            <div className={header.menu_container}>
-                <nav className={header.nav_container}>
-                    <ul className={header.nav_container__list}>
-                        <li>
+        <header className={`${header.header}`}>
+            <div className={`${header.header_inHeader}`}>
+                <div className={`${header.logo}`}>
+                    <Link
+                        to={'/'}
+                        className={`${header.logo__link}`}
+                    >
+                        <img
+                            className={`${header.logo__link__image}`}
+                            src="https://onlinegallery.art/_webblocks/images/logo.svg"
+                            alt="logo"
+                        />
+                    </Link>
+                </div>
+                <nav className={`${header.nav_container}`}>
+                    <ul className={`${header.nav_container__list}`}>
+                        <li className={`${header.desktop}`}>
                             <a
                                 href="#/"
-                                className={header.button}
-                            >Register</a>
+                                className={`${header.link}`}
+                            >Artworks</a>
                         </li>
-                        <li>
+                        <li className={`${header.desktop}`}>
                             <a
                                 href="#/"
-                                className={header.searchButton}
-                            >
-                                <img
-                                    className={header.searchButton__img}
-                                    src="./searchIcon.svg"
-                                    alt="search"
+                                className={`${header.link}`}
+                            >Artists</a>
+                        </li>
+                        <li className={`${header.desktop}`}>
+                            <a
+                                href="#/"
+                                className={`${header.link}`}
+                            >Auctions</a>
+                        </li>
+                        <li className={`${header.desktop}`}>
+                            <a
+                                href="#/"
+                                className={`${header.link}`}
+                            >News</a>
+                        </li>
+                        <li className={`${header.search_container}`}>
+                            <form action="" className={`${header.search}`}>
+                                <button
+                                    className={`${header.search__button}`}
+                                    type="submit"
+                                >
+                                    <img src="./searchIconGrey.svg" alt="search" />
+                                </button>
+                                <input
+                                    className={`${header.search__input}`}
+                                    type="search"
+                                    placeholder="Search..."
                                 />
-                            </a>
+                            </form>
                         </li>
                     </ul>
                 </nav>
-                <div>
-                    menu
+                <div className={`${header.menu_container}`}>
+                    <nav className={`${header.nav_container}`}>
+                        <ul className={`${header.nav_container__list}`}>
+                            <li>
+                                <a
+                                    href="#/"
+                                    className={`${header.link}`}
+                                >Pricing</a>
+                            </li>
+                            <li>
+                                <a
+                                    href="#/"
+                                    className={`${header.link}`}
+                                >Sell</a>
+                            </li>
+                            <li>
+                                <a
+                                    href="#/"
+                                    className={`${header.button}`}
+                                >Register</a>
+                            </li>
+                            <li>
+                                <a
+                                    href="#/"
+                                    className={`${header.link}`}
+                                >Login</a>
+                            </li>
+                            <li className={`${header.searchButton_container}`}>
+                                <a
+                                    href="#/"
+                                    className={`${header.searchButton}`}
+                                >
+                                    <img src="./searchIcon.svg" alt="search" />
+                                </a>
+                            </li>
+                        </ul>
+                    </nav>
+                    <a href="#/" className={`${header.menuButton}`}>
+                        <img src="./menuIcon.svg" alt="menu" />
+                    </a>
                 </div>
             </div>
         </header>
