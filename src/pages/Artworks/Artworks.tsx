970c923e7ab97a96e95646c5adc808cbd65cdf60
Kostya Kosyuk <kostya.kosyuk007@gmail.com>
2023-06-20 18:08:48 +0300

    masonry grid done, added react-masonry-css

--- src/pages/Artworks/Artworks.tsx
+++ src/pages/Artworks/Artworks.tsx
@@ -4,17 +4,31 @@ import DATA_test from "../../DATA_test.json";
 import { Artwork } from "../../components/Artwork";
 import { useParams } from "react-router-dom";
 import './Artworks.scss';
+import Masonry from 'react-masonry-css';
 
 export const Artworks: React.FC = () => {
   const { filter } = useParams();
 
+  const breakpointColumnsObj = {
+    default: 5,
+    1280: 4,
+    1024: 3,
+    768: 2
+  };
+
   return (
     <div className="artworks container">
-      {DATA_test.map((dataArt) => (
-        <React.Fragment key={dataArt.id}>
-          <Artwork dataArt={dataArt} />
-        </React.Fragment>
-      ))}
+      <Masonry
+        breakpointCols={breakpointColumnsObj}
+        className="my-masonry-grid"
+        columnClassName="my-masonry-grid_column"
+      >
+        {DATA_test.map((dataArt) => (
+          <React.Fragment key={dataArt.id}>
+            <Artwork dataArt={dataArt} />
+          </React.Fragment>
+        ))}
+      </Masonry>
     </div>
   );
 };
