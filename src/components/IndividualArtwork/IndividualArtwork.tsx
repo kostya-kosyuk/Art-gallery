cdd39190e137f36b41cd6d8450d7ccc566c2016c
Kostya Kosyuk <kostya.kosyuk007@gmail.com>
2023-07-06 14:37:05 +0300

    saving

--- src/components/IndividualArtwork/IndividualArtwork.tsx
+++ src/components/IndividualArtwork/IndividualArtwork.tsx
@@ -18,7 +18,7 @@ const IndividualArtwork = () => {
         <div className="artwork-container">
             <div className="mainInfo-container">
                 <div className="images">
-                    <div className="titleAndAuthor">
+                    <div className="titleAndAuthor mobile-only">
                         <div className="nameInfo">
                             <p className='nameInfo__name'>{artworkName}</p>
                             <p>By <a className='nameInfo__authorLink' href={authorLink}>{authorName}</a></p>
@@ -38,13 +38,29 @@ const IndividualArtwork = () => {
                         </div>
                     </div>
                 </div>
-                <div className='price'>
-                    <p>US$ {price}</p>
-                    <button className='button'>Add to cart</button>
-                </div>
+                <div className="info-container">
+                    <div className="titleAndAuthor not-mobile">
+                        <div className="nameInfo">
+                            <p className='nameInfo__name'>{artworkName}</p>
+                            <p>By <a className='nameInfo__authorLink' href={authorLink}>{authorName}</a></p>
+                        </div>
+                        <div className="buttonsContainer">
+                            <div onClick={handleActivateLike} className='likeContainer'>
+                                <svg className={classNames(isLike && 'active')}
+                                    xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                                    <path d="M20.84 4.58a5.5 5.5 0 0 0-7.78 0L12 5.66l-1.06-1.08a5.5 5.5 0 0 0-7.78 7.78L12 21.23l8.84-8.85a5.5 5.5 0 0 0 0-7.8z" />
+                                </svg>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='price'>
+                        <p>US$ {price}</p>
+                        <button className='button'>Add to cart</button>
+                    </div>
+                    <hr className='artwork-container__space' />
 
+                </div>
             </div>
-            <hr className='artwork-container__space'/>
         </div>
     );
 };
